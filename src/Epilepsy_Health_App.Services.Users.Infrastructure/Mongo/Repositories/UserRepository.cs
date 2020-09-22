ceb0573cede6188916bebe28727d711e5e342075
flapek <filap4@gmail.com>
2020-09-22 22:19:25 +0200

    add new method to interface

--- src/Epilepsy_Health_App.Services.Users.Infrastructure/Mongo/Repositories/UserRepository.cs
+++ src/Epilepsy_Health_App.Services.Users.Infrastructure/Mongo/Repositories/UserRepository.cs
@@ -1,7 +1,10 @@
+using Epilepsy_Health_App.Services.Users.Core.Entities;
 using Epilepsy_Health_App.Services.Users.Core.Repositories;
 using Epilepsy_Health_App.Services.Users.Infrastructure.Mongo.Documents;
 using Joint.DB.Mongo;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Epilepsy_Health_App.Services.Users.Infrastructure.Mongo.Repositories
 {
@@ -12,6 +15,36 @@ namespace Epilepsy_Health_App.Services.Users.Infrastructure.Mongo.Repositories
         public UserRepository(IMongoRepository<UserDocument, Guid> repository)
         {
             _repository = repository;
+        }
+
+        public Task<List<User>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAsync(List<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAsync(List<string> emails)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Guid id, User user)
+        {
+            throw new NotImplementedException();
         }
     }
 }
