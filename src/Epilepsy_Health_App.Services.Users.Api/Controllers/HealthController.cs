2235fcc2adbd25f2cf5e2aea9626d1aef2d4f45b
flapek <filap4@gmail.com>
2020-10-05 13:48:53 +0200

    add health check controller and update config

---
+++ src/Epilepsy_Health_App.Services.Users.Api/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Epilepsy_Health_App.Services.Users.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping() => Ok();
+    }
+}
